f70e6ef08a9bbb2947d0ebf120dc1b9fa93e86ab
Natalia <nataliamh.88@gmail.com>
2018-09-10 11:33:59 -0300

    Arreglo de personajes en otro archivo

--- src/components/Main.js
+++ src/components/Main.js
@@ -1,13 +1,9 @@
 import React, { Component } from 'react';
 import '../App.css';
+import {personajes} from './personajes'
 
 class Main extends Component {
   render() {
-    const personajes = [{nombre: 'Eric Cartman', foto: 'https://upload.wikimedia.org/wikipedia/en/7/77/EricCartman.png'
-}, {nombre: 'Stan Marsh', foto: 'https://upload.wikimedia.org/wikipedia/en/a/a7/StanMarsh.png'
-}, {nombre: 'Kyle Broflovski', foto: 'https://upload.wikimedia.org/wikipedia/en/2/25/KyleBroflovski.png'
-}, {nombre: 'Kenny McCormick', foto: 'https://vignette.wikia.nocookie.net/southpark/images/6/6f/KennyMcCormick.png/revision/latest?cb=20160409020502'
-}];
 let counter = 1;
     return (
       <div className='App-main'>
---
+++ src/components/personajes.js
@@ -0,0 +1,7 @@
+// el nombre del archivo puede ser en miniscula porque solo guardo una variable
+
+export const personajes = [{nombre: 'Eric Cartman', foto: 'https://upload.wikimedia.org/wikipedia/en/7/77/EricCartman.png'
+}, {nombre: 'Stan Marsh', foto: 'https://upload.wikimedia.org/wikipedia/en/a/a7/StanMarsh.png'
+}, {nombre: 'Kyle Broflovski', foto: 'https://upload.wikimedia.org/wikipedia/en/2/25/KyleBroflovski.png'
+}, {nombre: 'Kenny McCormick', foto: 'https://vignette.wikia.nocookie.net/southpark/images/6/6f/KennyMcCormick.png/revision/latest?cb=20160409020502'
+}];
